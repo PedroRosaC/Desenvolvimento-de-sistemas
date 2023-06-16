bb097c691b42745cb1c77b295d8569d8bdcdbcdb
Pedro <pedro.prccau@gmail.com>
2023-06-16 10:01:30 -0300

    Brasil é pica

---
+++ Paginasinterligadas/TablePage.php
@@ -0,0 +1,72 @@
+
+    <?php
+        require_once './shared/header.php'
+    ?>
+<div>
+    <script>
+        $(document).ready(function (){
+            $('#example').DataTable();
+        });
+        </script>
+    <table class="table table-responsive table-bordered table-striped table-primary">
+        <thead>
+            <tr>
+                <th>
+                    Nome
+                </th>
+                <th>
+                    Sobrenome
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                   oi
+                </td>
+                <td>
+                    tcvhau
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    fdfdsg
+                </td>
+                <td>
+                    sfdgfds
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    sgdffd
+                </td>
+                <td>
+                    sgdfdfs
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    sgdf
+                </td>
+                <td>
+                    gsfdgfds
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td>
+                
+                </td>
+            </tr>
+            
+        </tbody>
+    </table>
+</div>
+    <?php
+        require_once './shared/footer.php'
+    ?>
+
+
+
---
+++ Paginasinterligadas/shared/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <script src="../Js/jquery.dataTables.min.js" type="text/javascript"></sScript>
+        <script src="../Js/jquery-3.7.0.min.js" type="text/javascript"></script>
+        <script src="Js/bootstrap.bundle.min.js" type="text/javascript"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title>Página</title>
+        <style>
+            .col-md-12{
+                border: 1px solid;
+            }
+            p{
+                text-align: justify;
+            }
+        </style>
+        
+    </head>
+    <body class="container">
+        
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="nav nav-pills"> 
+                    <li class="nav-item">
+                        <a class="nav-link " href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="TablePage.php">Table Page</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="ModalPage.php">Modal Page</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="carroucelpage.php">carroucel sla oq</a>
+                    </li>
+                   
+                    
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+            
